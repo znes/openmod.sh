88d0bce6f20b218ccf8f361b08be1900b78c61d2
Hilpert <simon.hilpert@fh-flensburg.de>
2016-09-21 16:08:01 +0200

    Add sql snippet to extract region shapes from renpassgis db

---
+++ misc/extract-region-shapes.sql
@@ -0,0 +1,9 @@
+﻿SELECT region_key as region_key,
+    stat_levl_ as nuts_state_level,
+    u_region_id as nuts_id, 
+    ST_SetSRID(ST_AsText(geom), 4326) as wkt_polygon, 
+    ST_SetSRID(ST_AsText(geom_point), 4326) as wkt_point 
+FROM parameter_region, distribution_urid_to_nuts 
+WHERE u_region_id LIKE '%DEF%' AND  
+	u_region_id = distribution_urid_to_nuts.nuts_id;
+
