90e71d7edd01887f3d9884f96a7e37e1e7304b8d
Stephan Günther <gnn.code@gmail.com>
2016-09-21 16:08:01 +0200

    Add some more timing information

--- openmod/sh/network.py
+++ openmod/sh/network.py
@@ -9,6 +9,17 @@ import oemof.db as db
 
 from .schemas import dev as schema
 
+# This is debugging stuff that can be ignored.
+from datetime import datetime
+def stopwatch():
+    if not hasattr(stopwatch, "now"):
+        stopwatch.now = datetime.now()
+        return None
+    last = stopwatch.now
+    stopwatch.now = datetime.now()
+    return str(stopwatch.now-last)[0:-4]
+### Debugging stuff over.
+
 def from_db(debug=False):
     engine = db.engine("openMod.sh")
 
@@ -21,8 +32,15 @@ def from_db(debug=False):
     buses = {t: Bus(uid=t, type=t) for t in ["Gas", "Biomasse", nrg]}
     query = session.query(schema.Plant)
     count = query.count()
+    time = stopwatch()
+    perc = 0
     for i, p in enumerate(query.all()):
-        stdout.write("\r{0:5}: {1:6.2%}".format(i, i/count)) if debug else None
+        time = stopwatch() if not int(perc * 10) == int(10 * i/count) else time
+        perc = i/count
+        stdout.write("\r{0:5}: {1:6.2%} ({2} for last 10%)".format(
+            i,
+            perc,
+            time if time else "?")) if debug else None
         if p.type in ["Wasserkraft", "Windkraft", "Solarstrom"]:
             components.Source(uid=p.id, outputs=[buses[nrg]], val=p.feedin)
         elif p.type in ["Gas", "Biomasse"]:
