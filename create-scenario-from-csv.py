62e81c507e1805aedcf62a5c3b89fc10d9020def
cswh <clemens.wingenbach@uni-flensburg.de>
2016-09-21 16:08:01 +0200

    Add timeseries

--- create-scenario-from-csv.py
+++ create-scenario-from-csv.py
@@ -32,55 +32,58 @@ if len(scenario_pd) != 1:
     raise Exception("There schould be exactly one row. " + \
                     "Please specify only one scenario = one row")
 
+timeseries_pd = pd.read_csv(scenario + '-timeseries.scsv', sep=';')
 demand_pd = pd.read_csv(scenario + '-demand.scsv', sep=';')
 
-# This line is necessary so that flask-sqlalchemy creates a database session
-# for us.
-web.app.app_context().push()
+## This line is necessary so that flask-sqlalchemy creates a database session
+## for us.
+#web.app.app_context().push()
 
-# Let's store a shortcut to the session to save some typing.
-DB = osm.DB.session
+## Let's store a shortcut to the session to save some typing.
+#DB = osm.DB.session
 
-cs = osm.Changeset()
-DB.add(cs)
-DB.flush()
+#cs = osm.Changeset()
+#DB.add(cs)
+#DB.flush()
 
-uid = '1'
-csid = cs.id
-ts= datetime.now(tz.utc)
+#uid = '1'
+#csid = cs.id
+#ts= datetime.now(tz.utc)
 
-for i,r in scenario_pd.iterrows():
-    x = osm.Relation(uid = uid,
-                     changeset_id = csid,
-                     timestamp = ts,
-                     myid = r['name'],
-                     tags = {'type': 'scenario',
-                             'name': r['name'],
-                             'scenario_year': r['scenario_year'],
-                             'scenario_description': r['scenario_description']})
-    scenario_db = x
-    DB.add(x)
+## Scenario
+#for i,r in scenario_pd.iterrows():
+#    x = osm.Relation(uid = uid,
+#                     changeset_id = csid,
+#                     timestamp = ts,
+#                     myid = r['name'],
+#                     tags = {'type': 'scenario',
+#                             'name': r['name'],
+#                             'scenario_year': r['scenario_year'],
+#                             'scenario_description': r['scenario_description']})
+#    scenario_db = x
+#    DB.add(x)
 
-DB.flush()
+#DB.flush()
 
-hub_nodes = {}
-for i,r in demand_pd.iterrows():
-    x = osm.Node(uid = uid,
-                 changeset_id = csid,
-                 timestamp = ts,
-                 myid = r['name'],
-                 lon = r['lon'],
-                 lat = r['lat'],
-                 tags = {'type': 'demand',
-                         'oemof_class': 'sink',
-                         'name': r['name'],
-                         'energy_amount': r['energy_amount'],
-                         'energy_sector': r['energy_sector']},
-                 referencing_relations = [scenario_db])
-    for h in r['hubs'].split(','):
-        hub_nodes[h] = hub_nodes.get(h, []) + [x]
-    # add timeseries
-    DB.add(x)
+## Demand
+#hub_nodes = {}
+#for i,r in demand_pd.iterrows():
+#    x = osm.Node(uid = uid,
+#                 changeset_id = csid,
+#                 timestamp = ts,
+#                 myid = r['name'],
+#                 lon = r['lon'],
+#                 lat = r['lat'],
+#                 tags = {'type': 'demand',
+#                         'oemof_class': 'sink',
+#                         'name': r['name'],
+#                         'energy_amount': r['energy_amount'],
+#                         'energy_sector': r['energy_sector']},
+#                 referencing_relations = [scenario_db])
+#    for h in r['hubs'].split(','):
+#        hub_nodes[h] = hub_nodes.get(h, []) + [x]
+#    # add timeseries
+#    DB.add(x)
 
-DB.commit()
+#DB.commit()
 
