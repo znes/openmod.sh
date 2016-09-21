1354d77946f0fa76a9168128e2730735d38e1490
S. Günther <gnn.code@gmail.com>
2016-09-21 16:08:01 +0200

    Display popup when a feature is clicked
    The popup contains a table with information for the powerplants. A lot
    of this stuff is taken from the "OpenLayers" popup example, so stuff
    that's not working correctly (like the placing of the 'x' which closes
    the popup) is related not having enough time to fix everything after
    copy and pasting from the example.
    The fact that to plants are clickable links (used as pseudo buttons)
    prepares for future plotting functionality.

--- static/code.js
+++ static/code.js
@@ -11,11 +11,22 @@ $(document).ready(function() {
            success: function (d, _, _) {$.plot($("#timeseries-plot"), d);},
            dataType: "json"});
 
+  var popup = { close: $('#close-popup'), content: $('#popup-content'),
+                container: $('#popup') };
+  var overlay = new ol.Overlay({ element: popup.container.get(0),
+                                 autoPan: false });
+  popup.close.on('click', function (e) {
+    overlay.setPosition(undefined);
+    popup.close.blur();
+    return false;
+  });
+
   var map = new ol.Map({
     target: 'map',
     layers: [
       new ol.layer.Tile({source: new ol.source.Stamen({layer: 'watercolor'})})
     ],
+    overlays: [overlay],
     view: new ol.View({ // Center the viewport somewhere around the middle of SH.
                         center: ol.proj.transform([9.78, 54.17],
                                                   'EPSG:4326', 'EPSG:3857'),
@@ -40,7 +51,18 @@ $(document).ready(function() {
              // should so I settlet for this.
              map.on("click", function(e) {
                map.forEachFeatureAtPixel(e.pixel, function (feature, layer) {
-                 console.log("Clicked: " + feature);
+                 var position = feature.getGeometry().getCoordinates();
+                 var content = $(
+                     '<table><tr><th>EEG ID</th><th>capacity</th></tr></table>');
+                 $.each(feature.get("plants"), function (i, x) {
+                   var button = $(
+                       '<a class="plant-id" href="#">' + x.id + '</a>' );
+                   content.append($('<tr></tr>').append(
+                         $('<td></td>').append(button)).append(
+                         $("<td>" + x.capacity + "</td>")));
+                 });
+                 popup.content.html(content);
+                 overlay.setPosition(position);
                })});
              map.addLayer(layer, projections);
            },
