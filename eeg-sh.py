e51e99933667f2e4387ee76e16f50eaa09afe126
cswh <clemens.wingenbach@uni-flensburg.de>
2016-09-21 16:08:01 +0200

    Add working eeg script

--- eeg-sh.py
+++ eeg-sh.py
@@ -8,16 +8,14 @@ cs = osm.Changeset()
 db.add(cs)
 db.commit()
 
-x = pd.read_csv("renewable_power_plants_germany.csv")
+x = pd.read_csv("eeg-sh.csv")
 
 for i, r in x.iterrows():
-    print(i)
-    print(r)
-    node = osm.Node(r['lat'], r['lon'], 1, cs.id, myid="hereami",
-                    tags=[("area", "yes"), ("type", r["generator_type"])])
+    node = osm.Node(r['lat'], r['lon'], 1, cs.id,
+                    tags=[("area", "yes"), ("type", r["generation"]), ("installed_capacity",r["electrical"])])
     db.add(node)
     db.commit()
-    break
+    print(i)
 
 
                       
