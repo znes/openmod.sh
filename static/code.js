c8ace78446bd5643083b4795f2a1883c3391daeb
S. Günther <gnn.code@gmail.com>
2016-09-21 16:08:01 +0200

    Remove the 'md' and 'fl' overlays
    This is one part of a general move away from overlays and towards vector
    layers. The two overlays where just a hard coded proof of concept and
    are no longer necessary. As the first release  will focus on
    Schleswig-Holstein (SH) a point somewhere in the middle of SH is used as
    the new initial center of the map's viewport.
    The initial zoom level was also adjusted to be closer to the one in the
    first release.

--- static/code.js
+++ static/code.js
@@ -3,38 +3,18 @@ $(document).ready(function() {
            success: function (d, _, _) {$.plot($("#timeseries-plot"), d);},
            dataType: "json"});
 
-  var fl = ol.proj.transform([ 9.448389, 54.794287], 'EPSG:4326', 'EPSG:3857');
-  var md = ol.proj.transform([11.618492, 52.117532], 'EPSG:4326', 'EPSG:3857');
-
   var map = new ol.Map({
     target: 'map',
     layers: [
       new ol.layer.Tile({source: new ol.source.OSM()})
     ],
-    view: new ol.View({ center: md, zoom: 4 })
+    view: new ol.View({ // Center the viewport somewhere around the middle of SH.
+                        center: ol.proj.transform([9.78, 54.17],
+                                                  'EPSG:4326', 'EPSG:3857'),
+                        zoom: 7 })
   });
 
-  var md_marker = new ol.Overlay({
-    element: $('#marker').clone().attr("id", "md_marker"),
-    positioning: "center-center",
-    position: md });
-  var md_label = new ol.Overlay({
-    element: $('#label').clone().attr("id", "md_label").text("Magdeburg"),
-    positioning: "bottom-center",
-    position: md });
-  var fl_marker = new ol.Overlay({
-    element: $('#marker').clone().attr("id", "fl_marker"),
-    positioning: "center-center",
-    position: fl });
-  var fl_label = new ol.Overlay({
-    element: $('#label').clone().attr("id", "md_label").text("Flensburg"),
-    positioning: "bottom-center",
-    position: fl });
 
-  map.addOverlay(md_marker);
-  map.addOverlay(md_label);
-  map.addOverlay(fl_marker);
-  map.addOverlay(fl_label);
 
 });
 
