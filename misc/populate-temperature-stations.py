724641014f93281980195d5884c78e787ec746aa
Hilpert <simon.hilpert@fh-flensburg.de>
2016-09-21 16:08:01 +0200

    Add populate temperature station script

---
+++ misc/populate-temperature-stations.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+
+"""
+
+import sys
+import os.path
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+import pandas as pd
+from openmod.sh.schemas import dev as dev
+from openmod.sh import web
+
+# This line is necessary so that flask-sqlalchemy creates a database session
+# for us.
+web.app.app_context().push()
+
+# Let's store a shortcut to the session to save some typing.
+DB = dev.DB.session
+
+
+df = pd.read_csv('../data/temperature_stations.csv')
+
+for i,r in df.iterrows():
+    station  = dev.TemperatureStation(station_id=r['station_id'],
+                                      lon=r['lon'],
+                                      lat=r['lat'],
+                                      name=r['name'])
+    DB.add(station)
+    print('Commiting station {}.'.format(i))
+    DB.commit()
