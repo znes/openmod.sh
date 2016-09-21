d0e57a0749947fff273be9745f3c1bbb0cebe596
Stephan Günther <gnn.code@gmail.com>
2016-09-21 16:08:01 +0200

    Add working database access
    A little bit of a bigger commit but should be pretty self explanatory or
    at least accessible together with the Flask and SQLAlchemy
    documentation.
    The database access should work and display the correct counts for the
    objects in the two tables on the landing page (provided that the
    connection is configured correctly).

--- openmod.sh.py
+++ openmod.sh.py
@@ -1,5 +1,9 @@
 from flask import Flask
+from geoalchemy2 import Geometry
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
+import sqlalchemy as db
 
 app = Flask(__name__)
 
@@ -8,10 +12,41 @@ with open("uphpd") as f:
               zip(["user", "password", "host", "port", "database"],
                   filter(bool, f.read().splitlines()))}
 
+engine = db.create_engine(
+    "postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}".format(
+        **config))
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+Base = declarative_base()
+
+
+class Plant(Base):
+    __tablename__ = "plants"
+    __table_args__ = {"schema": "test"}
+    id = db.Column("id", db.String(), primary_key=True)
+    type = db.Column("type", db.String())
+    geometry = db.Column("geom", Geometry("POINT"))
+    capacity = db.Column("capacity", db.Integer())
+
+
+class Timeseries(Base):
+    __tablename__ = "timeseries"
+    __table_args__ = {"schema": "test"}
+    plant = db.Column("plantid", db.String(), primary_key=True)
+    step = db.Column("timestep", db.Integer, primary_key=True)
+    value = db.Column("value", db.Integer())
+
 
 @app.route('/')
 def root():
-    return "The openMod.sh landing page."
+    response = "The openMod.sh landing page.<br>|<br>"
+    plants = session.query(Plant).all()
+    series = session.query(Timeseries).all()
+    response += "| # of plants: {0}<br>| # of timeseries: {1}<br>|".format(
+        len(plants), len(series))
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True)
