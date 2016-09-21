0a7c88cee6f6f1f8d1b46ba0e9fd0f4683bf742f
Hilpert <simon.hilpert@fh-flensburg.de>
2016-09-21 16:08:01 +0200

    Correct region-shapes population

--- misc/populate-region-shapes.py
+++ misc/populate-region-shapes.py
@@ -20,22 +20,13 @@ web.app.app_context().push()
 # Let's store a shortcut to the session to save some typing.
 DB = dev.DB.session
 
-#df = pd.read_csv('../data/region-shapes-sh.csv', dtype={'region_key':str})
-#for i,r in df.iterrows():
-#    region = dev.Region(region_id=r['region_key'],
-#                        geom_polygon=r['wkt_polygon'],
-#geom_point=r['wkt_point'])
-#    DB.add(region)
-#    print('Commiting region {}.'.format(i))
-#    DB.commit()
-
-df = pd.read_csv('../data/temperature_stations.csv')
-
+df = pd.read_csv('../data/region-shapes-sh.csv', dtype={'region_key':str})
 for i,r in df.iterrows():
-    station  = dev.TemperatureStation(station_id=r['station_id'],
-                                      lon=r['lon'],
-                                      lat=r['lat'],
-                                      name=r['name'])
-    DB.add(station)
-    print('Commiting station {}.'.format(i))
+    region = dev.Region(region_id=r['region_key'],
+                        geom_polygon=r['wkt_polygon'],
+                        geom_point=r['wkt_point'])
+    DB.add(region)
+    print('Commiting region {}.'.format(i))
     DB.commit()
+
+
