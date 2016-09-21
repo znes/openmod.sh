4c070ae5eb0081aee03145591cfe6f9ed366566d
Hilpert <simon.hilpert@fh-flensburg.de>
2016-09-21 16:08:01 +0200

    Correct errors is voronoi python script

--- misc/voronoi.py
+++ misc/voronoi.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 
-uri = "postgresql+psycopg2://openmodsh_admin:op3nmod!sh@localhost:5434/openmodsh2"
+uri = "postgresql+psycopg2://openmodsh_admin:op3nmod!sh@localhost:5434/openmodsh"
 engine = create_engine(uri)
            
 # meta = MetaData(schema="voronoi")
@@ -16,8 +16,8 @@ print(x)
 
 sql = "\
 ALTER TABLE voronoi.temperature_station\
- ADD COLUMN geom_polygon geometry(Polygon,4326); \
+ ADD COLUMN geom_polygon geometry(MultiPolygon,4326); \
 UPDATE voronoi.temperature_station\
- SET geom_polygon = ST_SetSRID(ST_Voronoi(geom_point, null, 0.0, true), 4326);"
+ SET geom_polygon = ST_SetSRID(ST_CollectionExtract(ST_Voronoi(geom_point, null, 0.0, true),3), 4326);"
 x = engine.execute(sql)
 print(x)
