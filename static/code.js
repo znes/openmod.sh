23e8d909a477312cc0cbaa830ef8e2ee6818d824
Stephan Günther <gnn.code@gmail.com>
2016-09-21 16:08:01 +0200

    Add labels to markers
    Labels are simple <spans> for which a template has been created in the
    "#outworld", the label text is added dynamically after cloning the
    template and explicit 'positioning' attributes ensure that the overlays
    are properly positioned.

--- static/code.js
+++ static/code.js
@@ -16,13 +16,25 @@ $(document).ready(function() {
 
   var md_marker = new ol.Overlay({
     element: $('#marker').clone().attr("id", "md_marker"),
+    positioning: "center-center",
+    position: md });
+  var md_label = new ol.Overlay({
+    element: $('#label').clone().attr("id", "md_label").text("Magdeburg"),
+    positioning: "bottom-center",
     position: md });
   var fl_marker = new ol.Overlay({
     element: $('#marker').clone().attr("id", "fl_marker"),
+    positioning: "center-center",
+    position: fl });
+  var fl_label = new ol.Overlay({
+    element: $('#label').clone().attr("id", "md_label").text("Flensburg"),
+    positioning: "bottom-center",
     position: fl });
 
   map.addOverlay(md_marker);
+  map.addOverlay(md_label);
   map.addOverlay(fl_marker);
+  map.addOverlay(fl_label);
 
 });
 
