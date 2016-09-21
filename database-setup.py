8f07a2141b241c1e99911646316913f43bf461f4
Hilpert <simon.hilpert@fh-flensburg.de>
2016-09-21 16:08:01 +0200

    Add dev classes to database setup

--- database-setup.py
+++ database-setup.py
@@ -1,7 +1,10 @@
 import openmod.sh.schemas.osm as osm
+import openmod.sh.schemas.dev as dev
 from openmod.sh import web
 
 web.app.app_context().push()
 osm.DB.create_all()
 osm.DB.session.commit()
 
+dev.DB.create_all()
+dev.DB.session.commit()
