437fb7fdd29a11ad9ba1441ba605b345ccc3465f
Stephan Günther <gnn.code@gmail.com>
2016-09-21 16:08:01 +0200

    Refactor MD position into its own variable
    Keep it DRY

--- static/code.js
+++ static/code.js
@@ -3,22 +3,19 @@ $(document).ready(function() {
            success: function (d, _, _) {$.plot($("#timeseries-plot"), d);},
            dataType: "json"});
 
+  var md = ol.proj.transform([11.618492, 52.117532], 'EPSG:4326', 'EPSG:3857');
+
   var map = new ol.Map({
     target: 'map',
     layers: [
       new ol.layer.Tile({source: new ol.source.OSM()})
     ],
-    view: new ol.View({
-      center: ol.proj.transform([11.618492, 52.117532],
-                                'EPSG:4326', 'EPSG:3857'),
-      zoom: 4
-    })
+    view: new ol.View({ center: md, zoom: 4 })
   });
 
   var marker = new ol.Overlay({
     element: document.getElementById('marker'),
-    position: ol.proj.transform([11.618492, 52.117532],
-                                'EPSG:4326', 'EPSG:3857'),
+    position: md,
   });
 
   map.addOverlay(marker);
