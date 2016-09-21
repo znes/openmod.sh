1b4f7139591c5c444cb13e2b418875b890d7bcf9
cswh <clemens.wingenbach@uni-flensburg.de>
2016-09-21 16:08:01 +0200

    Add working voronoi script

--- misc/voronoi.py
+++ misc/voronoi.py
@@ -6,27 +6,52 @@ engine = create_engine(uri)
 # meta = MetaData(schema="voronoi")
 # meta.reflect(bind=engine)
 
+# TODO: make script shorter/combine sql commands...
+
 sql = "\
-ALTER TABLE voronoi.temperature_station\
- ADD COLUMN geom_point geometry(Point,4326); \
-UPDATE voronoi.temperature_station\
- SET geom_point = ST_SetSRID(ST_Point(lon, lat), 4326);"
+ALTER TABLE voronoi.temperature_station ADD COLUMN geom_point geometry(Point,4326);\
+UPDATE voronoi.temperature_station SET geom_point = ST_SetSRID(ST_Point(lon, lat), 4326);"
 x = engine.execute(sql)
 print(x)
 
+
+sql = "\
+CREATE TABLE voronoi.temperature_station_temp\
+  AS SELECT ST_Voronoi(mytable.mycolumn, null, 0.0, true) AS voronoi_collection\
+    FROM (SELECT ST_Collect(geom_point) AS mycolumn FROM voronoi.temperature_station) AS mytable;"
+
+x = engine.execute(sql)
+print(x)
 
 sql =  "\
 ALTER TABLE voronoi.temperature_station\
-    ALTER COLUMN geom_point TYPE geometry(MultiPoint,4326) USING ST_Multi(geom_point);"
+  ADD COLUMN voronoi geometry(Polygon, 4326);"
 
 x = engine.execute(sql)
 print(x)
 
-sql = "\
-ALTER TABLE voronoi.temperature_station\
- ADD COLUMN geom_polygon geometry(MultiPolygon,4326); \
-UPDATE voronoi.temperature_station\
- SET geom_polygon = ST_SetSRID(ST_CollectionExtract(ST_Voronoi(geom_point, null, 0.0, true), 3), 4326);"
+sql =  "\
+CREATE TABLE voronoi.temperature_station2 AS\
+  SELECT (ST_Dump(voronoi_collection)).geom AS voronoi FROM voronoi.temperature_station_temp;"
 
 x = engine.execute(sql)
 print(x)
+
+sql =  "\
+ALTER TABLE voronoi.temperature_station2\
+  ADD COLUMN voronoi_polygon geometry(POLYGON);\
+UPDATE voronoi.temperature_station2\
+  SET voronoi_polygon = ST_SetSRID(voronoi,4326);"
+
+x = engine.execute(sql)
+print(x)
+
+sql =  "\
+UPDATE voronoi.temperature_station \
+SET voronoi = temp.voronoi_polygon \
+FROM voronoi.temperature_station2 AS temp \
+WHERE ST_Contains(temp.voronoi_polygon, temperature_station.geom_point)"
+
+x = engine.execute(sql)
+print(x)
+
