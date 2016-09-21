680c4755d4ae6ee579e3642e48935016a02d2191
Stephan Günther <gnn.code@gmail.com>
2016-09-21 16:08:01 +0200

    Make `node` handling more robust
    A node's `id` is only created and stored if there isn't already one and
    tags get a default value so that the template doesn't complain.
    Pleas also note that the line containing `OSM` should've actually been
    part of the previous commit.
    Sorry for that.

--- openmod/sh/web.py
+++ openmod/sh/web.py
@@ -140,10 +140,13 @@ def osm_map():
     left, bottom, right, top = map(float, flask.request.args['bbox'].split(","))
     minx, maxx = sorted([top, bottom])
     miny, maxy = sorted([left, right])
-    nodes = [dict(id=id(n), **n)
-            for n in osm_map.nodes
+    nodes = [dict(**n)
+            for n in OSM.nodes
             for x, y in ((n["lat"], n["lon"]),)
             if minx <= x and  miny <= y and maxx >= x and maxy >= y]
+    for node in nodes:
+        node['id'] = node.get('id', id(node))
+        node['tags'] = node.get('tags', {})
     template = flask.render_template('map.xml', nodes=nodes,
                                           minlon=miny, maxlon=maxy,
                                           minlat=minx, maxlat=maxx)
