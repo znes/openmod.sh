ce208ba4403b50cfae827a6a1a1fed95e7306f3a
Stephan Günther <gnn.code@gmail.com>
2016-09-21 16:08:01 +0200

    Add a second marker
    This is a test/proof of concept that several markers based on one
    template can be added. Doing this had a few consequences though.
    
    The same marker can not be used twice so the template has to be cloned.
    This means that the template snippet will not be relocated and thus has
    to be put into an invisible "box", named 'outworld' here. The marker can
    not be made itself invisible because than it would also not be displayed
    on the map.
    
    Since cloning creates elements sharing the same 'id' which is something
    that shouldn't exist, the 'id' attributes had to be changed immediately
    after cloning the template. This also means that styling can no longer
    be done via id but has to be done via class.
    
    And of course the variable names for the overlays had to be adjusted.

--- static/code.js
+++ static/code.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
            success: function (d, _, _) {$.plot($("#timeseries-plot"), d);},
            dataType: "json"});
 
+  var fl = ol.proj.transform([ 9.448389, 54.794287], 'EPSG:4326', 'EPSG:3857');
   var md = ol.proj.transform([11.618492, 52.117532], 'EPSG:4326', 'EPSG:3857');
 
   var map = new ol.Map({
@@ -13,9 +14,15 @@ $(document).ready(function() {
     view: new ol.View({ center: md, zoom: 4 })
   });
 
-  var marker = new ol.Overlay({ element: $('#marker'), position: md });
+  var md_marker = new ol.Overlay({
+    element: $('#marker').clone().attr("id", "md_marker"),
+    position: md });
+  var fl_marker = new ol.Overlay({
+    element: $('#marker').clone().attr("id", "fl_marker"),
+    position: fl });
 
-  map.addOverlay(marker);
+  map.addOverlay(md_marker);
+  map.addOverlay(fl_marker);
 
 });
 
