d0ea55ef52883d3086545d35cc2b98a4fb800a0e
Stephan Günther <gnn.code@gmail.com>
2016-09-21 16:08:01 +0200

    Finally make 'redirect on form submit' work

--- openmod/sh/web.py
+++ openmod/sh/web.py
@@ -1,3 +1,4 @@
+from urllib.parse import urlparse, urljoin
 import itertools
 import json
 
@@ -7,6 +8,7 @@ import flask_cors as cors # TODO: Check whether the `@cors.cross_origin()`
                           #       decorators are still necessary once 'iD' is
                           #       served from within this app.
 import flask_login as fl
+import flask_wtf as wtfl
 import wtforms as wtf
 from geoalchemy2.functions import ST_AsGeoJSON as geojson
 from sqlalchemy.orm import sessionmaker
@@ -30,6 +32,56 @@ engine = db.engine("openMod.sh")
 
 Session = sessionmaker(bind=engine)
 session = Session()
+
+##### Safe Redirects ##########################################################
+#
+# If we allow redirects after form submission, we want it to be safe. The code
+# in here is taken from a [flask snippet][0], with minor modifications.
+#
+# This should probably go into it's own module but I'm putting it all here for
+# now, as some parts need to stay in this module while some parts can be
+# factored out later. The 'factoring out' part can be considered an open TODO.
+#
+# [0]: http://flask.pocoo.org/snippets/63/
+#
+##############################################################################
+
+app.config['WTF_CSRF_CHECK_DEFAULT'] = False
+
+csrf = wtfl.csrf.CsrfProtect(app)
+
+def is_safe_url(target):
+    ref_url = urlparse(flask.request.host_url)
+    test_url = urlparse(urljoin(flask.request.host_url, target))
+    return ((test_url.scheme in ('http', 'https')) and
+            (ref_url.netloc == test_url.netloc))
+
+    # TODO: Remove the `redirect_arg` parameter. That was only needed to
+    #       redirect to the 'oauth_callback' URL parameter, which isn't needed
+    #       anymore as redirection seems to be handled by flask-oauthlib there.
+def get_redirect_target(redirect_arg):
+    for target in flask.request.args.get(redirect_arg), flask.request.referrer:
+        if not target:
+            continue
+        if is_safe_url(target):
+            return target
+
+class RedirectForm(wtfl.Form):
+    next = wtf.HiddenField()
+
+    def __init__(self, *args, redirect_arg='next', **kwargs):
+        super().__init__(*args, **kwargs)
+        self.redirect_arg = redirect_arg
+        if not self.next.data:
+            self.next.data = get_redirect_target(redirect_arg) or ''
+
+    def redirect(self, endpoint='root', **values):
+        if is_safe_url(self.next.data):
+            return flask.redirect(self.next.data)
+        target = get_redirect_target(self.redirect_arg)
+        return flask.redirect(target or flask.url_for(endpoint, **values))
+
+##### Safe Redirects end here #################################################
 
 ##### User Management #########################################################
 #
@@ -70,16 +122,17 @@ login_manager.init_app(app)
 def load_user(user_id):
     return User.known.get(user_id)
 
-class Login(wtf.Form):
+class Login(RedirectForm):
     username = wtf.StringField('Username', [wtf.validators.Length(min=3,
                                                                   max=79)])
-    password = wtf.StringField('Password', [wtf.validators.Length(min=3,
-                                                                  max=79)])
+    password = wtf.PasswordField(
+            'Password', [wtf.validators.Length(min=3, max=79)])
 
+@csrf.exempt
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = Login(flask.request.form)
-    if flask.request.method == 'POST' and form.validate():
+    form = Login()
+    if form.validate_on_submit():
         user = load_user(form.username.data)
         if user is not None:
             if user.pw == form.password.data:
@@ -96,8 +149,7 @@ def login():
         # TODO: Doesn't seem to work, as `flask.request.args.get('next')` is
         #       always none. Have a look at http://flask.pocoo.org/snippets/63/
         #       for pointers on how to make this work.
-        redirect = flask.request.args.get('next')
-        return flask.redirect(redirect or flask.url_for('login'))
+        return form.redirect('login')
     return flask.render_template('login.html', form=form)
 
 @app.route('/logout')
@@ -323,7 +375,7 @@ def pre_request_debug_hook():
         "Endpt.: {0.endpoint}"]).format(flask.request, type(flask.request.data)))
 """
 
-class Authorize(wtf.Form):
+class Authorize(wtfl.Form):
      confirm = wtf.BooleanField('Authorize')
 
 @app.route('/iD/connection/oauth/authorize', methods=['GET', 'POST'])
