9653ef22e3715a2e930afe356796fa4536ed98ff
cswh <clemens.wingenbach@uni-flensburg.de>
2017-03-22 07:41:53 +0100

    Clean up helper.js

--- openmod/sh/static/helper.js
+++ openmod/sh/static/helper.js
@@ -1,26 +1,24 @@
 function alertModal(message, header = "", callback=function(){}) {
     $('#alertModal').remove();
     $(document.body).append(
-        '<div class="modal fade" id="alertModal" tabindex="-1" role="dialog" aria-labelledby="alertModalLabel" aria-hidden="true">' +
-        '  <div class="modal-dialog" role="document">' +
-        '    <div class="modal-content">' +
-        '      <div class="modal-header">' +
-        '        <h5 class="modal-title" id="alertModalLabel">' + header + '</h5>' +
-        '        <button type="button" class="close" data-dismiss="modal" aria-label="Close">' +
-        '          <span aria-hidden="true">&times;</span>' +
-        '        </button>' +
-        '      </div>' +
-        '      <div class="modal-body">' +
-        message +
-        '      </div>' +
-        '      <div class="modal-footer">' +
-        '        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>' +
-        '      </div>' +
-        '    </div>' +
-        '  </div>' +
-        '</div>');
+        '<div class="modal fade" id="alertModal" tabindex="-1" role="dialog" aria-labelledby="alertModalLabel" aria-hidden="true">'
+        +'<div class="modal-dialog" role="document">'
+            +'<div class="modal-content">'
+            +'<div class="modal-header">'
+                +'<h5 class="modal-title" id="alertModalLabel">' + header + '</h5>'
+                +'<button type="button" class="close" data-dismiss="modal" aria-label="Close">'
+                +'<span aria-hidden="true">&times;</span>'
+                +'</button>'
+            +'</div>'
+            +'<div class="modal-body">' + message + '</div>'
+            +'<div class="modal-footer">'
+                +'<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>'
+            +'</div>'
+            +'</div>'
+        +'</div>'
+        +'</div>');
     $('#alertModal').modal().show();
-    $('#confirmModal').on('hide.bs.modal', function(e) {
+    $('#alertModal').on('hide.bs.modal', function(e) {
         callback();
     });
 }
@@ -28,67 +26,63 @@ function alertModal(message, header = "", callback=function(){}) {
 function confirmModal(message, header = "", callback=function(){}) {
     $('#confirmModal').remove();
     $(document.body).append(
-        '<div class="modal fade" id="confirmModal" tabindex="-1" role="dialog" aria-labelledby="confirmModalLabel" aria-hidden="true">' +
-        '  <div class="modal-dialog" role="document">' +
-        '    <div class="modal-content">' +
-        '      <div class="modal-header">' +
-        '        <h5 class="modal-title" id="alertModalLabel">' + header + '</h5>' +
-        '        <button type="button" class="close" data-dismiss="modal" aria-label="Close">' +
-        '          <span aria-hidden="true">&times;</span>' +
-        '        </button>' +
-        '      </div>' +
-        '      <div class="modal-body">' +
-        message +
-        '      </div>' +
-        '      <div class="modal-footer">' +
-        '        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>' +
-        '        <button type="button" class="btn btn-primary confirm">Save changes</button>' +
-        '      </div>' +
-        '    </div>' +
-        '  </div>' +
-        '</div>');
+        '<div class="modal fade" id="confirmModal" tabindex="-1" role="dialog" aria-labelledby="conformModalLabel" aria-hidden="true">'
+        +'<div class="modal-dialog" role="document">'
+            +'<div class="modal-content">'
+            +'<div class="modal-header">'
+                +'<h5 class="modal-title" id="confirmModalLabel">' + header + '</h5>'
+                +'<button type="button" class="close" data-dismiss="modal" aria-label="Close">'
+                +'<span aria-hidden="true">&times;</span>'
+                +'</button>'
+            +'</div>'
+            +'<div class="modal-body">' + message + '</div>'
+            +'<div class="modal-footer">'
+                +'<button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>'
+                +'<button type="button" class="btn btn-secondary confirm" data-dismiss="modal">OK</button>'
+            +'</div>'
+            +'</div>'
+        +'</div>'
+        +'</div>');
     $('#confirmModal').modal().show();
     $('#confirmModal').on('hide.bs.modal', function(e) {
         callback(false);
     });
     $('#confirmModal .confirm').on('click', function(e) {
-        $('#confirmModal').unbind( 'hide.bs.modal' );
+        $('#confirmModal').unbind( 'hide.bs.modal');
         $('#confirmModal').modal('hide');
         callback(true);
     });
 }
 
-function promptModal(message, header = "", callback=function(){}) {
+function promptModal(text, defaultText="", header = "", callback=function(){}) {
     $('#promptModal').remove();
     $(document.body).append(
-        '<div class="modal fade" id="promptModal" tabindex="-1" role="dialog" aria-labelledby="confirmModalLabel" aria-hidden="true">' +
-        '  <div class="modal-dialog" role="document">' +
-        '    <div class="modal-content">' +
-        '      <div class="modal-header">' +
-        '        <h5 class="modal-title" id="alertModalLabel">' + header + '</h5>' +
-        '        <button type="button" class="close" data-dismiss="modal" aria-label="Close">' +
-        '          <span aria-hidden="true">&times;</span>' +
-        '        </button>' +
-        '      </div>' +
-        '      <div class="modal-body">' +
-                    message +
-            '      <div class="form-group">' +
-            '          <input type="text" class="form-control" placeholder="' + message + '">' +
-            '      </div>' +
-        '      </div>' +
-        '      <div class="modal-footer">' +
-        '        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>' +
-        '        <button type="button" class="btn btn-primary confirm">Save changes</button>' +
-        '      </div>' +
-        '    </div>' +
-        '  </div>' +
-        '</div>');
+        '<div class="modal fade" id="promptModal" tabindex="-1" role="dialog" aria-labelledby="promptModalLabel" aria-hidden="true">'
+        +'<div class="modal-dialog" role="document">'
+            +'<div class="modal-content">'
+            +'<div class="modal-header">'
+                +'<h5 class="modal-title" id="promptModalLabel">' + header + '</h5>'
+                +'<button type="button" class="close" data-dismiss="modal" aria-label="Close">'
+                +'<span aria-hidden="true">&times;</span>'
+                +'</button>'
+            +'</div>'
+            +'<div class="modal-body">' + text + '</div>'
+                +'<div class="form-group">'
+                    +'<input type="text" class="form-control" placeholder="' + defaultText + '">'
+                +'</div>'
+            +'<div class="modal-footer">'
+                +'<button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>'
+                +'<button type="button" class="btn btn-secondary confirm" data-dismiss="modal">OK</button>'
+            +'</div>'
+            +'</div>'
+        +'</div>'
+        +'</div>');
     $('#promptModal').modal().show();
     $('#promptModal').on('hide.bs.modal', function(e) {
-        callback("");
+        callback(null);
     });
     $('#promptModal .confirm').on('click', function(e) {
-        $('#promptModal').unbind( 'hide.bs.modal' );
+        $('#promptModal').unbind( 'hide.bs.modal');
         $('#promptModal').modal('hide');
         callback($('#promptModal').find('input').val());
     });
