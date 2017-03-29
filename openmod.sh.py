e77fff5d25fe7a9feccbe611b6763cce8820f984
cswh <clemens.wingenbach@uni-flensburg.de>
2017-03-29 18:43:23 +0200

    Add edit_scenario template to files being watched by flask for changes

--- openmod.sh.py
+++ openmod.sh.py
@@ -2,4 +2,5 @@ if __name__ == '__main__':
     import openmod.sh.gui
     from oemof.db import config as cfg
     webport = cfg.get('openMod.sh R/W', 'webport')
-    openmod.sh.gui.app.run(host="0.0.0.0", port=int(webport), debug=True)
+    openmod.sh.gui.app.run(host="0.0.0.0", port=int(webport), debug=True,
+                           extra_files=['openmod/sh/templates/edit_scenario.html'])
