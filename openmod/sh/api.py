52bd743188710c35bf79060532ac6661ce3bce57
Stephan Günther <gnn.code@gmail.com>
2017-04-11 20:39:44 +0200

    Fix `get_flow_result` ambiguities

--- openmod/sh/api.py
+++ openmod/sh/api.py
@@ -563,32 +563,29 @@ def get_flow_result(scenario_identifier, predecessor_name, successor_name,
         if by == 'name':
           scenario = (session
               .query(schema.Element)
-              .filter(schema.Element.name.like(scenario_identifier))
-              .first())
+              .filter(schema.Element.name == scenario_identifier)
+              .one())
           scenario_id = scenario.id
 
         else:
             scenario_id = scenario_identifier
 
-        predecessor = (session
-                       .query(schema.Element)
-                       .filter(schema.Element.name.like(predecessor_name))
-                       .first())
-        successor = (session
-                     .query(schema.Element)
-                     .filter(schema.Element.name.like(successor_name))
-                     .first())
+        predecessor = (scenario
+                       .query_children
+                       .filter(schema.Element.name == predecessor_name)
+                       .one())
+        successor = (scenario
+                     .query_children
+                     .filter(schema.Element.name == successor_name)
+                     .one())
         flow_result = (session
                        .query(schema.ResultSequences)
                        .filter_by(scenario_id=scenario_id,
                                   predecessor_id=predecessor.id,
                                   successor_id=successor.id)
-                   .first())
+                   .one())
 
-        if flow_result:
-            return flow_result.value
-        else:
-            return False
+        return flow_result.value
 
 def get_flow_results(scenario_identifier, by='id', subset='false'):
     """
