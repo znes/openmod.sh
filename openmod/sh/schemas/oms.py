5f528a25c622d9287a36d75009111297057be642
Stephan Günther <gnn.code@gmail.com>
2017-04-11 19:00:25 +0200

    Make SQLAlchemy happy
    Simme says there can only be one parent in each of these relationships,
    so it should be OK to set those flags. And `delete-orphan` doesn't work
    without them.

--- openmod/sh/schemas/oms.py
+++ openmod/sh/schemas/oms.py
@@ -156,17 +156,20 @@ class ResultSequences(DB.Model):
     scenario_id = DB.Column(DB.Integer, DB.ForeignKey(Element.id))
     scenario = DB.relationship(Element, foreign_keys=[scenario_id],
                                uselist=False,
-                               cascade='all, delete, delete-orphan')
+                               cascade='all, delete, delete-orphan',
+                               single_parent=True)
 
     predecessor_id = DB.Column(DB.Integer, DB.ForeignKey(Element.id))
     predecessor = DB.relationship(Element, foreign_keys=[predecessor_id],
                                   uselist=False,
-                                  cascade='all, delete, delete-orphan')
+                                  cascade='all, delete, delete-orphan'
+                                  single_parent=True)
 
     successor_id = DB.Column(DB.Integer, DB.ForeignKey(Element.id))
     successor = DB.relationship(Element, foreign_keys=[successor_id],
                                 uselist=False,
-                                cascade='all, delete, delete-orphan')
+                                cascade='all, delete, delete-orphan'
+                                single_parent=True)
 
     type = DB.Column(DB.String(255))
     value = DB.Column(ARRAY(DB.Float, dimensions=1), nullable=False)
